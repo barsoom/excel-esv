654a1a531fc54af2d2a71626372f9a7606ad3a3f
Olle Jonsson <olle.jonsson@auctionet.com>
2023-02-15 14:44:42 +0100

    Nest specs

--- spec/esv_spec.rb
+++ spec/esv_spec.rb
@@ -1,114 +1,116 @@
 require "esv"
 
-RSpec.describe ESV, ".generate and .parse" do
-  it "works" do
-    data = ESV.generate do |esv|
-      esv << [ "Dogs", "Cats" ]
-      esv << [ 1, 2 ]
+RSpec.describe ESV do
+  describe ".generate and .parse" do
+    it "works" do
+      data = ESV.generate do |esv|
+        esv << [ "Dogs", "Cats" ]
+        esv << [ 1, 2 ]
+      end
+
+      output = ESV.parse(data)
+
+      expect(output).to eq [
+        [ "Dogs", "Cats" ],
+        [ 1, 2 ],
+      ]
+    end
+  end
+
+  describe ".parse" do
+    it "raises if there's more than one worksheet" do
+      excel_file_with_two_worksheets = generate_excel_file do |sheet, book|
+        book.create_worksheet
+      end
+
+      expect {
+        ESV.parse(excel_file_with_two_worksheets)
+      }.to raise_error(/Expected 1 worksheet, found 2/)
     end
 
-    output = ESV.parse(data)
+    it "ignores formatting, always returning a plain array of data" do
+      excel_file_with_formatting = generate_excel_file do |sheet|
+        sheet.row(0).replace([ 1, 2 ])
+        sheet.row(0).default_format = Spreadsheet::Format.new(color: :blue)
+      end
 
-    expect(output).to eq [
-      [ "Dogs", "Cats" ],
-      [ 1, 2 ],
-    ]
-  end
-end
+      output = ESV.parse(excel_file_with_formatting)
 
-RSpec.describe ESV, ".parse" do
-  it "raises if there's more than one worksheet" do
-    excel_file_with_two_worksheets = generate_excel_file do |sheet, book|
-      book.create_worksheet
+      expect(output).to eq [
+        [ 1, 2 ],
+      ]
+
+      expect(output[0].class).to eq Array
     end
 
-    expect {
-      ESV.parse(excel_file_with_two_worksheets)
-    }.to raise_error(/Expected 1 worksheet, found 2/)
-  end
+    it "returns the last value of a formula cell" do
+      excel_file_with_formula = generate_excel_file do |sheet|
+        formula = Spreadsheet::Formula.new
+        formula.value = "two"
+        sheet.row(0).replace([ "one", formula ])
+      end
 
-  it "ignores formatting, always returning a plain array of data" do
-    excel_file_with_formatting = generate_excel_file do |sheet|
-      sheet.row(0).replace([ 1, 2 ])
-      sheet.row(0).default_format = Spreadsheet::Format.new(color: :blue)
+      output = ESV.parse(excel_file_with_formula)
+
+      expect(output).to eq [
+        [ "one", "two" ],
+      ]
+      expect(output[0].class).to eq Array
     end
 
-    output = ESV.parse(excel_file_with_formatting)
 
-    expect(output).to eq [
-      [ 1, 2 ],
-    ]
+    it "returns the URL of a link cell" do
+      excel_file_with_link = generate_excel_file do |sheet|
+        link = Spreadsheet::Link.new("https://example.com", "desc", "foo")
+        sheet.row(0).replace([ "one", link ])
+      end
+      output = ESV.parse(excel_file_with_link)
+      expect(output).to eq [
+        [ "one", "https://example.com#foo" ],
+      ]
 
-    expect(output[0].class).to eq Array
-  end
-
-  it "returns the last value of a formula cell" do
-    excel_file_with_formula = generate_excel_file do |sheet|
-      formula = Spreadsheet::Formula.new
-      formula.value = "two"
-      sheet.row(0).replace([ "one", formula ])
+      expect(output[0][1].class).to eq String
     end
 
-    output = ESV.parse(excel_file_with_formula)
+    private
 
-    expect(output).to eq [
-      [ "one", "two" ],
-    ]
-    expect(output[0].class).to eq Array
-  end
+    def generate_excel_file(&block)
+      book = Spreadsheet::Workbook.new
+      sheet = book.create_worksheet
 
+      block.call(sheet, book)
 
-  it "returns the URL of a link cell" do
-    excel_file_with_link = generate_excel_file do |sheet|
-      link = Spreadsheet::Link.new("https://example.com", "desc", "foo")
-      sheet.row(0).replace([ "one", link ])
+      data = ""
+      fake_file = StringIO.new(data)
+      book.write(fake_file)
+      data
     end
-    output = ESV.parse(excel_file_with_link)
-    expect(output).to eq [
-      [ "one", "https://example.com#foo" ],
-    ]
-
-    expect(output[0][1].class).to eq String
   end
 
-  private
-
-  def generate_excel_file(&block)
-    book = Spreadsheet::Workbook.new
-    sheet = book.create_worksheet
-
-    block.call(sheet, book)
-
-    data = ""
-    fake_file = StringIO.new(data)
-    book.write(fake_file)
-    data
-  end
-end
-
-RSpec.describe ESV, ".generate_file and .parse_file" do
-  before do
-    @file = Tempfile.new("esv")
-  end
-
-  it "works" do
-    path = @file.path
-
-    ESV.generate_file(path) do |esv|
-      esv << [ "Dogs", "Cats" ]
-      esv << [ 1, 2 ]
+  describe ".generate_file and .parse_file" do
+    before do
+      @file = Tempfile.new("esv")
     end
 
-    output = ESV.parse_file(path)
+    it "works" do
+      path = @file.path
 
-    expect(output).to eq [
-      [ "Dogs", "Cats" ],
-      [ 1, 2 ],
-    ]
-  end
+      ESV.generate_file(path) do |esv|
+        esv << [ "Dogs", "Cats" ]
+        esv << [ 1, 2 ]
+      end
 
-  after do
-    @file.close
-    @file.unlink
+      output = ESV.parse_file(path)
+
+      expect(output).to eq [
+        [ "Dogs", "Cats" ],
+        [ 1, 2 ],
+      ]
+    end
+
+    after do
+      @file.close
+      @file.unlink
+    end
   end
 end
